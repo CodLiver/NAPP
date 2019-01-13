a52fa732eaf058d6adf17838bc5b58d1032298cd
Cod Liver (C E S) <18186030+CodLiver@users.noreply.github.com>
2019-01-13 18:09:39 +0000

    Update solution-2.c

--- solution-2.c
+++ solution-2.c
@@ -188,10 +188,10 @@ double lennardJones(double distance, double x) {
 		return 0;
 	}*/
 	double distDx = x / (distance*distance);
-	double e = 1.65e-21;
+	//double e = 1.65e-21;
 	double mult = 3.4e-10*distDx;
 	double mult6 = mult*mult*mult*mult*mult*mult;
-	return (48 * e*mult6*distDx)*(mult6 - 0.5);
+	return (7.92e-20*mult6*distDx)*(mult6 - 0.5);
 		
 	//return (-4 * 3.4e-10*x)*(-12 * pow((1.65e-21*x/ (distance*distance)), 12) + 6 * pow((1.65e-21 *x / (distance*distance)), 6)) / (distance*distance);
 
